18c8dbb3924065c65d076d88a2423194eeb1c5e6
nahoang <nahoang9698@gmail.com>
2022-11-16 23:06:34 +0700

    Initial source

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Layout = ({children}) => {
+  const nav = () => (
+    <ul className="nav">
+      <li className="nav-item"><a className="nav-link"  href="">Home</a></li>
+      <li className="nav-item"><a className="nav-link" href="">Login</a></li>
+      <li className="nav-item"><a className="nav-link" href="">Register</a></li>
+    </ul>
+  )
+  
+
+  return <React.Fragment>
+    {nav()} {children}
+  </React.Fragment>
+}
+
+export default Layout;
