26ce7c7cf4d83ba6b44eae13ae30e6bee22fd6ca
nahoang <nahoang9698@gmail.com>
2022-11-16 23:30:23 +0700

    Add Head section

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import Head from 'next/head';
+
 const Layout = ({children}) => {
+
+  const head = () => (
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
+  )
+
   const nav = () => (
-    <ul className="nav">
-      <li className="nav-item"><a className="nav-link"  href="">Home</a></li>
-      <li className="nav-item"><a className="nav-link" href="">Login</a></li>
-      <li className="nav-item"><a className="nav-link" href="">Register</a></li>
+    <ul className="nav nav-tabs bg-warning">
+      <li className="nav-item"><a className="nav-link text-dark"  href="">Home</a></li>
+      <li className="nav-item"><a className="nav-link text-dark" href="">Login</a></li>
+      <li className="nav-item"><a className="nav-link text-dark" href="">Register</a></li>
     </ul>
   )
   
 
   return <React.Fragment>
-    {nav()} {children}
+    {head()}{nav()} <div className="container pt-5 pb-5">{children}</div>
   </React.Fragment>
 }
 
