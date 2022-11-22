b8b06e98cf2d5561f3826153ca6fede104e88228
nahoang <nahoang9698@gmail.com>
2022-11-22 22:45:13 +0700

    feat(server): Implement login function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,15 +3,16 @@ const express = require('express')
 const router = express.Router()
 
 //import validators
-const { userRegisterValidator } = require('../validators/auth');
+const { userRegisterValidator, userLoginValidator } = require('../validators/auth');
 const { runValidation } = require('../validators/index');
 
 
 // import from controller
 
-const { register, registerActivate } = require('../controllers/auth');
+const { register, registerActivate, login } = require('../controllers/auth');
 
 router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);
+router.post('/login', userLoginValidator, runValidation, login);
 
 module.exports = router;
