629ecbe85a181c25a9bae277b40f3c2704076e7b
nahoang <nahoang9698@gmail.com>
2022-12-28 16:05:43 +0700

    feat(server): Uploading image to aws s3

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,28 +1,86 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
+const formidable = require('formidable');
+const uuidv4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+
+//s3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+})
 
 
+
+// exports.create = (req, res) => {
+//   //
+//   const {name, content} = req.body;
+//   const slug = slugify(name)
+//   const image = {
+//     url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
+//     key: '123'
+//   };
+
+//   const category = new Category({name, slug, image, content});
+//   category.postedBy = req.user._id;
+
+//   category.save((err, data) => {
+//     if (err) {
+//       console.log('CATEGORY CREATE ERR', err)
+//       return res.status(400).json({
+//         error: 'Category create failed'
+//       })
+//     }
+//     res.json(data);
+//   })
+// }
 
 exports.create = (req, res) => {
-  //
-  const {name, content} = req.body;
-  const slug = slugify(name)
-  const image = {
-    url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
-    key: '123'
-  };
-
-  const category = new Category({name, slug, image, content});
-  category.postedBy = req.user._id;
-
-  category.save((err, data) => {
-    if (err) {
-      console.log('CATEGORY CREATE ERR', err)
+  let form = new formidable.IncomingForm()
+  form.parse(req, (err, fields, files) => {
+    if(err) {
       return res.status(400).json({
-        error: 'Category create failed'
+        error: "Image could not upload"
       })
     }
-    res.json(data);
+    // console.table({err, fields, files})
+    const {name, content} = fields
+    const {image} = files
+
+    const slug = slugify(name);
+    let category = new Category({name, content, slug});
+    if (image.size > 2000000) {
+      return res.status(400).json({
+        error: "Image should be less than 2mb"
+      })
+    }
+    //upload image to s3
+    const params = {
+      Bucket: 'hackr-nahoang',
+      Key: `category/${uuidv4()}`,
+      Body: image.path,
+      ACL: 'public-read',
+      ContentType: `image/jpg`
+    }
+
+    s3.upload(params, (err, data) => {
+      if(err) {
+        console.log(err);
+        res.status(400).json({error: 'Upload to s3 failed'})
+      }
+      console.log('AWS UPLOAD RES DATA', data)
+      // category.image.url = data.Location
+      // category.image.key = data.Key
+
+      // // save to db
+      // category.save((err, success) => {
+      //   if (err) {
+      //     res.status(400).json({error: 'Error saving category to db'})
+      //   }
+      //   return res.json(success)
+      // })
+    })
   })
 }
 
