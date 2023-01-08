c55e8088d03823ea4dc4d722bf1e45778eb0b3db
nahoang <nahoang9698@gmail.com>
2023-01-08 16:50:01 +0700

    wip

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -89,8 +89,14 @@ exports.create = (req, res) => {
 };
 
 exports.list = (req, res) => {
-  //
-  
+  Category.find({}).exec((err, data) => {
+    if (err) {
+        return res.status(400).json({
+            error: 'Categories could not load'
+        });
+    }
+    res.json(data);
+  })
 }
 
 exports.read = (req, res) => {
